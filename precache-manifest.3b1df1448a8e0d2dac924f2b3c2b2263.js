9e4b36ea3051cce18a96a1e4af830f9b1a714a54
Yurii Kichalo <yuriykichalo@gmail.com>
2019-10-25 18:18:26 +0300

    Updates

---
+++ precache-manifest.3b1df1448a8e0d2dac924f2b3c2b2263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d62860a13e6f64348639dfbb228b61c",
+    "url": "/SignatureList/index.html"
+  },
+  {
+    "revision": "409ff4d079bf94c941b8",
+    "url": "/SignatureList/static/css/main.fcd30f1e.chunk.css"
+  },
+  {
+    "revision": "60d2ddc68e1208c0d6af",
+    "url": "/SignatureList/static/js/2.edf6ef3e.chunk.js"
+  },
+  {
+    "revision": "409ff4d079bf94c941b8",
+    "url": "/SignatureList/static/js/main.baba1ed9.chunk.js"
+  },
+  {
+    "revision": "fb5bf46dd05ab6fa43d3",
+    "url": "/SignatureList/static/js/runtime-main.69e19c50.js"
+  }
+]);
